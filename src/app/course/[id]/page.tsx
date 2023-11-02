e4e2a56def16afac6630a9ec2a1062fc0e0ed721
Vito Medlej <rip.vitto@gmail.com>
2023-11-02 11:06:53 +0200

    weird

--- src/app/course/[id]/page.tsx
+++ src/app/course/[id]/page.tsx
@@ -9,115 +9,114 @@ import courses from '../../../../courses.json'
 
 const Index = ({ params }: { params: any | { slug: any } }) => {
   
-
+  
+  console.log('courses: ', courses);
  
   console.log('params: ', params?.id);
   const router = useRouter() 
   const [course,setCourse] = useState<any>(null)
+  console.log('course: ', course);
 
-  const selector = () => {
-    try {
+  // const selector = () => {
+  //   try {
       
-      const crs = courses?.find(i=>String(i.id) === String(params?.id))
-      if (crs) {
-        setCourse(crs)
-      }
-    }
-    catch(e) {
-      console.log('e: ', e);
+  //     const crs = courses?.find(i=>String(i.id) === String(params?.id))
+  //     if (crs) {
+  //       setCourse(crs)
+  //     }
+  //   }
+  //   catch(e) {
+  //     console.log('e: ', e);
       
-    }
-  }
-  useEffect(() => {
-    if (courses && params?.id){
+  //   }
+  // }
+  // useEffect(() => {
+  //   if (courses && params?.id){
 
-      selector()
-    }
-  }, [params?.id])
+  //     selector()
+  //   }
+  // }, [params?.id])
   
 
   if (!course || !courses) return <Box>Course Not Found</Box>
-  try {
   return (
-    <Grid maxWidth={'xl'} container sx={{my:12}}>
+    // <Grid maxWidth={'xl'} container sx={{my:12}}>
     
-        <Grid item xs={12} sm={8}>
-            <Title>{course?.name}</Title>
+    //     <Grid item xs={12} sm={8}>
+    //         <Title>{course?.name}</Title>
              
-            <ContentList summaryContent={course?.summaryContent ? course?.summaryContent : ['']}/>
+    //         <ContentList summaryContent={course?.summaryContent ? course?.summaryContent : ['']}/>
                 
             
-        </Grid>
-        <Grid item xs={12} sm={4}>
-            <Box className=' auto' sx={{width:{xs:'95%',sm:'350px'},
-          px:1,py:2.5,
-          boxShadow: `rgba(0, 0, 0, 0.16) 0px 1px 4px`,
-          borderRadius:3,
-          }} >
-              <Box sx={{height:'250px'}}>
-                <img
-                style={{borderRadius:0}}
-                src={`${course?.img}`}
+    //     </Grid>
+    //     <Grid item xs={12} sm={4}>
+    //         <Box className=' auto' sx={{width:{xs:'95%',sm:'350px'},
+    //       px:1,py:2.5,
+    //       boxShadow: `rgba(0, 0, 0, 0.16) 0px 1px 4px`,
+    //       borderRadius:3,
+    //       }} >
+    //           <Box sx={{height:'250px'}}>
+    //             <img
+    //             style={{borderRadius:0}}
+    //             src={`${course?.img}`}
                 
-                alt="" className="img contain" />
-              </Box>
-              <Box className='flex col' sx={{mt:1}}>
-              <Typography sx={{color:'black'}}>
+    //             alt="" className="img contain" />
+    //           </Box>
+    //           <Box className='flex col' sx={{mt:1}}>
+    //           <Typography sx={{color:'black'}}>
 
-                <strong>Prices:</strong>
-              </Typography>
-              {/* <Typography sx={{color:'black'}}>
+    //             <strong>Prices:</strong>
+    //           </Typography>
+    //           {/* <Typography sx={{color:'black'}}>
 
-                  1 Person: ${course.price}
-                </Typography>
-                <Typography sx={{color:'black'}}>
-                  1-5 Persons: $50
-                </Typography>
-                <Typography sx={{color:'black'}}>
-                  5 & above Persons: $40
-                </Typography> */}
-              <Typography sx={{color:'black'}}>
+    //               1 Person: ${course.price}
+    //             </Typography>
+    //             <Typography sx={{color:'black'}}>
+    //               1-5 Persons: $50
+    //             </Typography>
+    //             <Typography sx={{color:'black'}}>
+    //               5 & above Persons: $40
+    //             </Typography> */}
+    //           <Typography sx={{color:'black'}}>
 
-                {
-                  course?.coursePrice['1 person']
-                }
-                </Typography>
-                <Divider sx={{my:1}} light></Divider>
-                <Box>
-                      <Typography sx={{color:'black'}}>
-                <strong>Duration:</strong>
-                {' '}
-              {course?.duration}  
+    //             {
+    //               course?.coursePrice['1 person']
+    //             }
+    //             </Typography>
+    //             <Divider sx={{my:1}} light></Divider>
+    //             <Box>
+    //                   <Typography sx={{color:'black'}}>
+    //             <strong>Duration:</strong>
+    //             {' '}
+    //           {course?.duration}  
           
               
-              </Typography>
-                </Box>
-                 <Divider sx={{my:1}} light></Divider>
-                <Box >
+    //           </Typography>
+    //             </Box>
+    //              <Divider sx={{my:1}} light></Divider>
+    //             <Box >
 
-                      <Typography sx={{color:'black'}}>
+    //                   <Typography sx={{color:'black'}}>
 
-                <strong>Software ver. pref.:</strong>
-                {' '}
-               {course?.softwareVersionPreference}
-              </Typography>
-                </Box>
-                <Btn sx={{mt:2}}>
-                  Buy NOW
-                </Btn>
-              </Box>
-              {/* <Box>
+    //             <strong>Software ver. pref.:</strong>
+    //             {' '}
+    //            {course?.softwareVersionPreference}
+    //           </Typography>
+    //             </Box>
+    //             <Btn sx={{mt:2}}>
+    //               Buy NOW
+    //             </Btn>
+    //           </Box>
+    //           {/* <Box>
 
-              </Box> */}
-            </Box>
-          </Grid>
-    </Grid>
+    //           </Box> */}
+    //         </Box>
+    //       </Grid>
+    // </Grid>
+    <h1>abc</h1>
   )
-}
-catch (e){
-  console.log('e: allll', e);
 
-}
+
 }
 
 export default Index
