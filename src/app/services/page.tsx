2869c1ab73027b1b0df529aa1b7852e60f97bf9a
Vito Medlej <rip.vitto@gmail.com>
2023-11-30 13:19:45 +0200

    serv

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -33,28 +33,28 @@ const Page = () => {
 
 
     },
-    {
-      title:  text('Personalized Guidance', 'محتوى موثوق به'),
-      desc:
-          text('Led by Dr. Rana CHEHAB, our elite team provides personalized education. Join us for hands-on courses tailored to your success.', 'تم إنشاؤها بواسطة خبراء، مكتبة الدروس والتمارين الموثوقة في Khan Academy تغطي مواضيع الرياضيات والعلوم وأكثر. دائمًا مجانية للمتعلمين والمعلمين.')
-        ,
-        icon: 'https://cdn.kastatic.org/images/lohp/empower_teachers_icon.png',
+//     {
+//       title:  text('Personalized Guidance', 'محتوى موثوق به'),
+//       desc:
+//           text('Led by Dr. Rana CHEHAB, our elite team provides personalized education. Join us for hands-on courses tailored to your success.', 'تم إنشاؤها بواسطة خبراء، مكتبة الدروس والتمارين الموثوقة في Khan Academy تغطي مواضيع الرياضيات والعلوم وأكثر. دائمًا مجانية للمتعلمين والمعلمين.')
+//         ,
+//         icon: 'https://cdn.kastatic.org/images/lohp/empower_teachers_icon.png',
 
-  },    
-  {
-    title: text('Service 1', 'أسعار معقولة'),
-    desc:  text('17 years of excellence. Specialized programs. Practical learning. Choose us for unparalleled expertise in translation and training.', 'يمارس الطلاب بوتيرة خاصة بهم، يقومون أولاً بسد الفجوات في فهمهم ثم يسرعون في عملية التعلم.'),
-    icon: 'https://cdn.kastatic.org/images/lohp/empower_teachers_icon.png',
+//   },    
+//   {
+//     title: text('Service 1', 'أسعار معقولة'),
+//     desc:  text('17 years of excellence. Specialized programs. Practical learning. Choose us for unparalleled expertise in translation and training.', 'يمارس الطلاب بوتيرة خاصة بهم، يقومون أولاً بسد الفجوات في فهمهم ثم يسرعون في عملية التعلم.'),
+//     icon: 'https://cdn.kastatic.org/images/lohp/empower_teachers_icon.png',
   
-},
-  {
-      title:
-          text('Your Success, Our Focus', 'مدرسون معتمدون')
-        ,
-      desc: text('Your success matters. Explore specialized courses, learn from industry experts, and earn international accreditation. Enroll now for a successful journey.', 'مع Khan Academy، يمكن للمعلمين تحديد الفجوات في فهم طلابهم، ضبط التعليم، وتلبية احتياجات كل طالب.'),
-      icon: 'https://cdn.kastatic.org/images/lohp/personalized_learning_icon.png',
+// },
+//   {
+//       title:
+//           text('Your Success, Our Focus', 'مدرسون معتمدون')
+//         ,
+//       desc: text('Your success matters. Explore specialized courses, learn from industry experts, and earn international accreditation. Enroll now for a successful journey.', 'مع Khan Academy، يمكن للمعلمين تحديد الفجوات في فهم طلابهم، ضبط التعليم، وتلبية احتياجات كل طالب.'),
+//       icon: 'https://cdn.kastatic.org/images/lohp/personalized_learning_icon.png',
 
-  }
+//   }
 ]
 
   return (
